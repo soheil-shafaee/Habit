5324d27a9c92f65cdc634fe1e1d1d03d6f00ac07
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2023-06-30 13:29:50 +0430

    Add intoduce text to app

--- login.py
+++ login.py
@@ -1,8 +1,12 @@
 from tkinter import *
+import time
 
+# ------ Constant
 BACKGROUND_COLOR = "#33FFCC"
 FONT_COLOR = "#330099"
-
+INTRODUCE_TEXT = "Are you looking to build positive habits and track your progress over time? Look no further than'\n' " \
+                 "HabitApp! Our innovative application is designed to help you develop and maintain healthy habits,'\n' " \
+                 "with the added feature of integrating with Pixela graph for visualizing your progress."
 # ------- Background Section --------
 window = Tk()
 window.title("Habit Pixel")
@@ -11,18 +15,36 @@ window.geometry("400x450")
 window.config(background=BACKGROUND_COLOR)
 window.iconbitmap("hobby.ico")
 
-# -------- Introduce Section --------
+# -------- Welcome Section --------
+habit_image = PhotoImage(file='images/image-a.png')
 canvas = Canvas(width=500, height=500, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.place(x=1, y=1)
 # --------- Image Section -------------
-habit_image = PhotoImage(file='images/image-a.png')
 canvas.create_image(200, 250, image=habit_image)
-
 text = Label(
-             text="Welcome to the ",
-             font=("Monotype Corsiva", 25, "bold"),
-             fg=FONT_COLOR,
-             background=BACKGROUND_COLOR)
-text.place(x=100, y=150)
+    text="Welcome to '\n' a Powerful Tool '\n' to Track and Improve Your",
+    font=("Monotype Corsiva", 25, "bold"),
+    fg=FONT_COLOR,
+    background=BACKGROUND_COLOR)
 
+text.place(x=10, y=80)
+
+
+def clean_page():
+    canvas.destroy()
+    text.destroy()
+
+
+window.after(5000, clean_page)
+
+
+def intro_text():
+    inro_text = Label(text=INTRODUCE_TEXT,
+                      font=("Monotype Corsiva", 14, "bold"),
+                      fg=FONT_COLOR,
+                      background=BACKGROUND_COLOR)
+    inro_text.place(x=10, y=10)
+
+
+window.after(6000, intro_text)
 window.mainloop()
