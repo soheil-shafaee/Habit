c5f23dced3a3876423ab430e91f44843c1a56563
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2023-07-01 12:49:12 +0430

    Add introduce text

--- login.py
+++ login.py
@@ -4,9 +4,12 @@ import time
 # ------ Constant
 BACKGROUND_COLOR = "#33FFCC"
 FONT_COLOR = "#330099"
-INTRODUCE_TEXT = "Are you looking to build positive habits and track your progress over time? Look no further than'\n' " \
-                 "HabitApp! Our innovative application is designed to help you develop and maintain healthy habits,'\n' " \
-                 "with the added feature of integrating with Pixela graph for visualizing your progress."
+INTRODUCE_TEXT = "Are you looking to build positive \n habits and track \n your progress over time? Look no further " \
+                 "than\n " \
+                 "HabitApp!\n Our innovative application is designed to \n help you develop and maintain healthy " \
+                 "habits," \
+                 "\n " \
+                 "with the added feature of integrating with \n Pixela graph for visualizing your progress."
 # ------- Background Section --------
 window = Tk()
 window.title("Habit Pixel")
@@ -18,11 +21,11 @@ window.iconbitmap("hobby.ico")
 # -------- Welcome Section --------
 habit_image = PhotoImage(file='images/image-a.png')
 canvas = Canvas(width=500, height=500, bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.place(x=1, y=1)
+canvas.place(x=5, y=5)
 # --------- Image Section -------------
-canvas.create_image(200, 250, image=habit_image)
+canvas.create_image(200, 300, image=habit_image)
 text = Label(
-    text="Welcome to '\n' a Powerful Tool '\n' to Track and Improve Your",
+    text="Welcome to \n a Powerful Tool \n to Track and Improve Your",
     font=("Monotype Corsiva", 25, "bold"),
     fg=FONT_COLOR,
     background=BACKGROUND_COLOR)
@@ -30,21 +33,39 @@ text = Label(
 text.place(x=10, y=80)
 
 
+# -------- Cleaning Page -----------
 def clean_page():
-    canvas.destroy()
     text.destroy()
+    canvas.destroy()
 
 
-window.after(5000, clean_page)
+window.after(1000, clean_page)
 
 
-def intro_text():
+def intr_text():
     inro_text = Label(text=INTRODUCE_TEXT,
                       font=("Monotype Corsiva", 14, "bold"),
                       fg=FONT_COLOR,
                       background=BACKGROUND_COLOR)
-    inro_text.place(x=10, y=10)
+
+    def cleaning():
+        btn_go.destroy()
+        inro_text.destroy()
+
+    btn_go = Button(
+        text="Let's Go",
+        width=10,
+        background=FONT_COLOR,
+        fg=BACKGROUND_COLOR,
+        command=cleaning
+    )
+    inro_text.place(x=10, y=120)
+    btn_go.place(x=150, y=320)
 
 
-window.after(6000, intro_text)
+window.after(2000, intr_text)
+
+# -------- Cleaning Page -----------
+
+
 window.mainloop()
